0e385ebb8bca753d69751dea27e1306db993de7e
Tamkien Cao <kiencthe140322@fpt.edu.vn>
2021-03-18 15:49:32 +0700

    Added main components for fragment_main.xml

--- app/src/main/java/com/fpt/vinmartauth/ProfileFragment.java
+++ app/src/main/java/com/fpt/vinmartauth/ProfileFragment.java
@@ -1,13 +1,18 @@
 package com.fpt.vinmartauth;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.fpt.vinmartauth.login.LoginActivity;
 
 import java.util.ArrayList;
 
@@ -30,17 +35,13 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        // Inflate the layout for this fragment
+        TextView tvLogIn = view.findViewById(R.id.tvLogin);
+        tvLogIn.setOnClickListener(view1 -> {
+            Intent i = new Intent(this.getContext(), LoginActivity.class);
+            startActivity(i);
+        });
         ArrayList<ProfileMenuItem> items = new ArrayList<>();
         RecyclerView rv = view.findViewById(R.id.rcvProfileMenu);
-        items.add(new ProfileMenuItem(R.drawable.ic_baseline_edit_24, getString(R.string.edit_profile)));
-        items.add(new ProfileMenuItem(R.drawable.ic_baseline_location_on_24, getString(R.string.shipping_address)));
-        items.add(new ProfileMenuItem(R.drawable.ic_baseline_favorite_border_24, getString(R.string.favorite)));
-        items.add(new ProfileMenuItem(R.drawable.ic_baseline_history_24, getString(R.string.order_history)));
-        items.add(new ProfileMenuItem(R.drawable.ic_baseline_local_shipping_24, getString(R.string.track_order)));
-        items.add(new ProfileMenuItem(R.drawable.ic_baseline_credit_card_24, getString(R.string.cards)));
-        items.add(new ProfileMenuItem(R.drawable.ic_baseline_notifications_24, getString(R.string.notifications)));
-        items.add(new ProfileMenuItem(R.drawable.ic_baseline_exit_to_app_24, getString(R.string.log_out)));
         items.add(new ProfileMenuItem(R.drawable.ic_baseline_edit_24, getString(R.string.edit_profile)));
         items.add(new ProfileMenuItem(R.drawable.ic_baseline_location_on_24, getString(R.string.shipping_address)));
         items.add(new ProfileMenuItem(R.drawable.ic_baseline_favorite_border_24, getString(R.string.favorite)));
