bacb2a9747067f9071fceb1f8b96cad99b6286bc
Cao Tam Kiên <kiencthe140322@fpt.edu.vn>
2021-03-24 00:51:54 +0700

    Revert "Feature/firebase (#33)"
    This reverts commit 3641858e9c99ab1c4d172f6cd31f2524ef22d04a.

--- app/src/main/java/com/fpt/vinmartauth/login/SignupActivity.java
+++ app/src/main/java/com/fpt/vinmartauth/login/SignupActivity.java
@@ -1,8 +1,6 @@
-package com.fpt.vinmartauth.view;
+package com.fpt.vinmartauth.login;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,14 +13,7 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-
         ImageView btnBack = findViewById(R.id.btnBack);
         btnBack.setOnClickListener(view -> finish());
-
-        Button btnSignUp = findViewById(R.id.btnSignUp);
-        btnSignUp.setOnClickListener(view -> {
-            Intent intent = new Intent(this, VerificationActivity.class);
-            startActivity(intent);
-        });
     }
 }
