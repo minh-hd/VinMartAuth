39b946657e39a5e052a52b19a970172fb449b415
Tamkien Cao <kiencthe140322@fpt.edu.vn>
2021-03-28 20:36:13 +0700

    edit ViewByCategoryActivity.kt

--- app/src/main/java/com/fpt/vinmartauth/view/productview/ViewByCategoryActivity.kt
+++ app/src/main/java/com/fpt/vinmartauth/view/productview/ViewByCategoryActivity.kt
@@ -5,6 +5,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.fpt.vinmartauth.R
+import com.fpt.vinmartauth.model.ProductModel
 
 class ViewByCategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,8 @@ class ViewByCategoryActivity : AppCompatActivity() {
         setContentView(R.layout.activity_view_by_category)
         findViewById<TextView>(R.id.tvCategoryTitle).text = intent.getStringExtra("CategoryName")
         val catID = intent.getStringExtra("CategoryID")
+        val productModel = ProductModel()
+        productModel.getProductByCategory(catID)
         findViewById<ImageView>(R.id.imageView5).setOnClickListener {
             finish()
         }
