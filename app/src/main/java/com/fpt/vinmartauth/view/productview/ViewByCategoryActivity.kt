192da9e5969451e5ef5440f5c43bfbc9cc025dc9
Tamkien Cao <kiencthe140322@fpt.edu.vn>
2021-03-28 07:07:19 +0700

    optimized product fragment

--- app/src/main/java/com/fpt/vinmartauth/view/productview/ViewByCategoryActivity.kt
+++ app/src/main/java/com/fpt/vinmartauth/view/productview/ViewByCategoryActivity.kt
@@ -2,6 +2,7 @@ package com.fpt.vinmartauth.view.productview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import com.fpt.vinmartauth.R
 
@@ -10,5 +11,8 @@ class ViewByCategoryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_by_category)
         findViewById<TextView>(R.id.tvCategoryTitle).text = intent.getStringExtra("CategoryName")
+        findViewById<ImageView>(R.id.imageView5).setOnClickListener {
+            finish()
+        }
     }
 }
