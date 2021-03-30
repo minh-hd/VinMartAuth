9345b8472db4adca854b32bef1c61f21d3d0e625
Tamkien Cao <kiencthe140322@fpt.edu.vn>
2021-03-30 17:02:59 +0700

    minor fix

--- app/src/main/java/com/fpt/vinmartauth/entity/Customer.java
+++ app/src/main/java/com/fpt/vinmartauth/entity/Customer.java
@@ -1,97 +1,95 @@
 package com.fpt.vinmartauth.entity;
 
-import androidx.annotation.RecentlyNonNull;
-
 import java.util.Date;
 
 public class Customer {
-  private String fullName;
-  private String phone;
-  private String email;
-  private String password;
-  private Enum<Gender> gender;
-  private Date dateOfBirth;
-  private String profileImageLink;
+    private String fullName;
+    private String phone;
+    private String email;
+    private String password;
+    private Enum<Gender> gender;
+    private Date dateOfBirth;
+    private String profileImageLink;
 
-  public Customer() {
-  }
+    public Customer() {
+    }
 
-  public Customer(String fullName, String phone, String email, String password, Enum<Gender> gender, Date dateOfBirth, String profileImageLink) {
-    this.fullName = fullName;
-    this.phone = phone;
-    this.email = email;
-    this.password = password;
-    this.gender = gender;
-    this.dateOfBirth = dateOfBirth;
-    this.profileImageLink = profileImageLink;
-  }
+    public Customer(String fullName, String phone, String email, String password, Enum<Gender> gender, Date dateOfBirth, String profileImageLink) {
+        this.fullName = fullName;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.profileImageLink = profileImageLink;
+    }
 
-  public String getFullName() {
-    return fullName;
-  }
+    public String getFullName() {
+        return fullName;
+    }
 
-  public void setFullName(String fullName) {
-    this.fullName = fullName;
-  }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 
-  public String getPhone() {
-    return phone;
-  }
+    public String getPhone() {
+        return phone;
+    }
 
-  public void setPhone(String phone) {
-    this.phone = phone;
-  }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public String getPassword() {
-    return password;
-  }
+    public String getPassword() {
+        return password;
+    }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-  public Enum<Gender> getGender() {
-    return gender;
-  }
+    public Enum<Gender> getGender() {
+        return gender;
+    }
 
-  public void setGender(Enum<Gender> gender) {
-    this.gender = gender;
-  }
+    public void setGender(Enum<Gender> gender) {
+        this.gender = gender;
+    }
 
-  public Date getDateOfBirth() {
-    return dateOfBirth;
-  }
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
 
-  public void setDateOfBirth(Date dateOfBirth) {
-    this.dateOfBirth = dateOfBirth;
-  }
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 
-  public String getProfileImageLink() {
-    return profileImageLink;
-  }
+    public String getProfileImageLink() {
+        return profileImageLink;
+    }
 
-  public void setProfileImageLink(String profileImageLink) {
-    this.profileImageLink = profileImageLink;
-  }
+    public void setProfileImageLink(String profileImageLink) {
+        this.profileImageLink = profileImageLink;
+    }
 
-  @Override
-  public String toString() {
-    return "Customer{" +
-            "fullName='" + fullName + '\'' +
-            ", phone='" + phone + '\'' +
-            ", email='" + email + '\'' +
-            ", password='" + password + '\'' +
-            ", gender=" + gender +
-            ", dateOfBirth=" + dateOfBirth +
-            ", profileImageLink='" + profileImageLink + '\'' +
-            '}';
-  }
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "fullName='" + fullName + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", gender=" + gender +
+                ", dateOfBirth=" + dateOfBirth +
+                ", profileImageLink='" + profileImageLink + '\'' +
+                '}';
+    }
 }
