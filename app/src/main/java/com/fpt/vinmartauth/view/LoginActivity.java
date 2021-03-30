4db33375bdaaaedf6a4b42aec8d764fae68b974d
Hoàng Duy Minh <27907165+Sprollie@users.noreply.github.com>
2021-03-30 22:17:56 +0700

    Merge minhhd
    * Change application name
    
    * Add authentication validation
    
    * Modify login screen
    
    * Add Customer model
    
    * Add user authentication
    
    * Refactor project structure
    
    * Add FirestoreInstance
    
    * Refactor FirestoreInstance
    
    * Add loading dialog
    
    * Edit Sign In behavior
    
    * Edit login text
    
    * Add theme for authentication EditText
    
    * Fix get login status
    
    * Edit build.gradle
    
    * Add toString for Customer
    
    * Edit login activity screen
    
    * Implement login via Firebase
    
    * Dismiss login loading dialog
    
    * Keep screen on when loading
    
    * Add edit profile screen
    
    * Edit reset password screen
    
    * Edit sign up screen
    
    * Add internal storage permissions
    
    * Delete redundant classes
    
    * Move authentication functions to controller
    
    * Add some libraries
    
    * Add new Edit profile activity
    
    * Edit component ids
    
    * Add sign in icon
    
    * Refactor login function
    
    * Delete model
    
    * Refactor user functions
    
    * Add recyclerview items event
    
    * Load component to view
    
    * Rebuild reset password
    
    * Replace loading text
    
    * Move sign up function to controller
    
    * Delete Customer
    
    * Fix email validation
    
    * Fix screen redirect
    
    * Fix UI

--- app/src/main/java/com/fpt/vinmartauth/view/LoginActivity.java
+++ app/src/main/java/com/fpt/vinmartauth/view/LoginActivity.java
@@ -3,9 +3,6 @@ package com.fpt.vinmartauth.view;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,9 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.fpt.vinmartauth.R;
 import com.fpt.vinmartauth.validation.AuthValidation;
-import com.fpt.vinmartauth.view.dialog.LoadingDialog;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.fpt.vinmartauth.view.authentication.AuthenticationController;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -30,46 +25,20 @@ public class LoginActivity extends AppCompatActivity {
   private TextView tvSignUp;
   private EditText edtEmail;
   private EditText edtPassword;
-  private LoadingDialog loadingDialog;
-  private FirebaseAuth mAuth;
-  private FirebaseAuth.AuthStateListener mAuthListener;
-
-  @Override
-  protected void onStart() {
-    super.onStart();
-    mAuth.addAuthStateListener(mAuthListener);
-  }
-
-  @Override
-  protected void onStop() {
-    super.onStop();
-    if (mAuthListener != null) {
-      mAuth.removeAuthStateListener(mAuthListener);
-    }
-  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
 
-    loadingDialog = new LoadingDialog(this);
-
-    mAuth = FirebaseAuth.getInstance();
-    mAuthListener = firebaseAuth -> {
-      FirebaseUser user = firebaseAuth.getCurrentUser();
-      if (user != null) {
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
-      }
-    };
-
+    edtEmail = findViewById(R.id.editTextTextEmailAddress);
+    edtPassword = findViewById(R.id.editTextTextPassword);
     btnBack = findViewById(R.id.btnBack);
     btnBack.setOnClickListener(view -> finish());
 
     btnSignIn = findViewById(R.id.btnHomeSignIn);
     btnSignIn.setOnClickListener(view -> {
-      String email = edtEmail.getText().toString().trim();
+      String email = edtEmail.getText().toString().toLowerCase().trim();
       String password = edtPassword.getText().toString().trim();
       if (!authValidation.isEmail(email)) {
         edtEmail.setError("Email không hợp lệ");
@@ -82,19 +51,7 @@ public class LoginActivity extends AppCompatActivity {
 
       InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
       inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-      loadingDialog.startLoadingDialog();
-      mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
-        loadingDialog.dismissDialog();
-        if (task.isSuccessful()) {
-          Log.i("Login", "OK!");
-          Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-          startActivity(intent);
-        } else {
-          Log.i("Login", "Failed!");
-          Toast toast = Toast.makeText(LoginActivity.this, "Sai email hoặc mật khẩu", Toast.LENGTH_LONG);
-          toast.show();
-        }
-      });
+      new AuthenticationController().loginWithEmail(this, email, password);
 
     });
 
@@ -110,37 +67,6 @@ public class LoginActivity extends AppCompatActivity {
       startActivity(intent);
     });
 
-    edtEmail = findViewById(R.id.editTextTextEmailAddress);
-    edtPassword = findViewById(R.id.editTextTextPassword);
-    TextWatcher textWatcher = new TextWatcher() {
-      @Override
-      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        edtEmail.setError(null);
-        edtPassword.setError(null);
-      }
-
-      @Override
-      public void onTextChanged(CharSequence s, int start, int before, int count) {
-        String email = edtEmail.getText().toString().trim();
-        String password = edtPassword.getText().toString().trim();
-
-        if (email.equals("")) {
-          edtEmail.setError(null);
-        }
-
-        if (password.equals("")) {
-          edtPassword.setError(null);
-        }
-
-      }
-
-      @Override
-      public void afterTextChanged(Editable s) {
-      }
-    };
-    edtEmail.addTextChangedListener(textWatcher);
-
-    edtPassword.addTextChangedListener(textWatcher);
   }
 
 
